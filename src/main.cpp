c528421d61654965e419fed06b83f2cdf1d3eb25
Mabry Cervin <mpcervin@uncg.edu>
2015-09-13 20:20:54 +0000

    Fixed tabs not converting to spaces

--- src/main.cpp
+++ src/main.cpp
@@ -20,77 +20,77 @@ SDL_Event event;
 
 // Program entry point
 int main(int argc, char* argv[]) {
-	if (init() == false) {
-		return 1;
-	}
+    if (init() == false) {
+        return 1;
+    }
 
-	TextSpec* textspec = new TextSpec;
-	if (load(textspec) == false) {
-		return 1;
-	}
+    TextSpec* textspec = new TextSpec;
+    if (load(textspec) == false) {
+        return 1;
+    }
 
-	// Main event loop
-	bool quit = false;
+    // Main event loop
+    bool quit = false;
 
-	while (quit == false) {
-		while (SDL_PollEvent(&event)) {
-			if (event.type == SDL_KEYDOWN) {
-				if (event.key.keysym.sym == SDLK_UP) {
-					quit = true;
-				}
-			}
-		}
+    while (quit == false) {
+        while (SDL_PollEvent(&event)) {
+            if (event.type == SDL_KEYDOWN) {
+                if (event.key.keysym.sym == SDLK_UP) {
+                    quit = true;
+                }
+            }
+        }
 
-		// Wait 5ms
-		SDL_Delay(5);
-	}
+        // Wait 5ms
+        SDL_Delay(5);
+    }
 
-	delete textspec;
-	SDL_FreeSurface(screen);
+    delete textspec;
+    SDL_FreeSurface(screen);
     SDL_Quit();
     return 0;
 }
 
 // Initializes SDL and subsystems
 bool init() {
-	// Try to initialize all SDL subsystems
-	if(SDL_Init(SDL_INIT_EVERYTHING) == -1) {
-		cout << "Could not initialize SDL" << endl;
-		return false;
-	}
+    // Try to initialize all SDL subsystems
+    if(SDL_Init(SDL_INIT_EVERYTHING) == -1) {
+        cout << "Could not initialize SDL" << endl;
+        return false;
+    }
 
-	// The primary drawing surface that gets flipped
-	screen = SDL_SetVideoMode(SCREEN_WIDTH, SCREEN_HEIGHT, SCREEN_BPP, SDL_SWSURFACE | SDL_FULLSCREEN);
+    // The primary drawing surface that gets flipped
+    screen = SDL_SetVideoMode(SCREEN_WIDTH, SCREEN_HEIGHT, SCREEN_BPP, SDL_SWSURFACE | SDL_FULLSCREEN);
 
-	// Check that `screen` was created correctly
-	if (screen == NULL) {
-		cout << "Primary screen surface could not be created" << endl;
-		return false;
-	}
-	
-	// Initialize SDL_TTF
-	if (TTF_Init() == -1) {
-		cout << "Could not load SDL_ttf" << endl;
-		return false;
-	}
+    // Check that `screen` was created correctly
+    if (screen == NULL) {
+        cout << "Primary screen surface could not be created" << endl;
+        return false;
+    }
+    
+    // Initialize SDL_TTF
+    if (TTF_Init() == -1) {
+        cout << "Could not load SDL_ttf" << endl;
+        return false;
+    }
 
-	// If we got here, everything worked
-	return true;
+    // If we got here, everything worked
+    return true;
 }
 
 bool load(TextSpec* textspec) {
-	textspec->bigFont = TTF_OpenFont("Anonymous_Pro.ttf", 16);
-	textspec->smallFont = TTF_OpenFont("Anonymous_Pro.ttf", 14);
+    textspec->bigFont = TTF_OpenFont("Anonymous_Pro.ttf", 16);
+    textspec->smallFont = TTF_OpenFont("Anonymous_Pro.ttf", 14);
 
-	if (textspec->bigFont == NULL && textspec->smallFont == NULL) {
-		cout << "Could not load font" << endl;
-		return false;
-	}
+    if (textspec->bigFont == NULL && textspec->smallFont == NULL) {
+        cout << "Could not load font" << endl;
+        return false;
+    }
 
-	textspec->textColor = {255, 255, 255};
-	textspec->bgActive = {80, 80, 80};
-	textspec->bgInactive = {0, 0, 0};
-	textspec->bgEmph = {60, 60, 60};
-	return true;
+    textspec->textColor = {255, 255, 255};
+    textspec->bgActive = {80, 80, 80};
+    textspec->bgInactive = {0, 0, 0};
+    textspec->bgEmph = {60, 60, 60};
+    return true;
 }
 
--- src/ui.h
+++ src/ui.h
@@ -12,36 +12,36 @@ enum Input {Up, Down};
 class TextSpec {
     public:
         TTF_Font* bigFont;
-	    TTF_Font* smallFont;
-	    SDL_Color textColor;
-    	SDL_Color bgActive;
-	    SDL_Color bgInactive;
-		SDL_Color bgEmph;
+        TTF_Font* smallFont;
+        SDL_Color textColor;
+        SDL_Color bgActive;
+        SDL_Color bgInactive;
+        SDL_Color bgEmph;
 };
 
 // Interface for all ui elements
 class UIElement {
     public:
         virtual ~UIElement();
-		virtual void render(SDL_Surface*, TextSpec&) = 0;
-		virtual void update(State*) = 0;
-		virtual void giveFocus() = 0;
-		virtual void takeFocus() = 0;
-		virtual void input(Input) = 0;
+        virtual void render(SDL_Surface*, TextSpec&) = 0;
+        virtual void update(State*) = 0;
+        virtual void giveFocus() = 0;
+        virtual void takeFocus() = 0;
+        virtual void input(Input) = 0;
 };
 
 class UIPlayback: public UIElement {
     public:
         UIPlayback();
-		~UIPlayback();
-		void render(SDL_Surface*, TextSpec&);
-		void update(State*);
-		void giveFocus();
-		void takeFocus();
-		void input(Input);
+        ~UIPlayback();
+        void render(SDL_Surface*, TextSpec&);
+        void update(State*);
+        void giveFocus();
+        void takeFocus();
+        void input(Input);
     private:
         Playback playState;
-		string name;
+        string name;
 };
 
 #endif
